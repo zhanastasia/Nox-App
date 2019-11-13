4da5e300028f8bfc93ccfd2d6bf4da8d390ea3d0
Anastasia <aeterna_nox@ukr.net>
2019-11-13 15:04:56 +0200

    US-135: Code review - Fixed issues

--- src/app/features/movies/movies.component.ts
+++ src/app/features/movies/movies.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Movie } from '../../models/movie.model';
+import { Movie } from '../models/movie.model';
 
 @Component({
    selector: 'app-movies',
