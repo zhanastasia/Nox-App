f7195b30e32610753d70b69c861f6a0c0bcf8b7d
Anastasia <aeterna_nox@ukr.net>
2019-11-15 12:10:44 +0200

    US-135: Code review - Added Lazy Loading

---
+++ src/app/features/movies/movie-routing.module.ts
@@ -0,0 +1,11 @@
+import { MoviesComponent } from './components/movies.component';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [{ path: '', component: MoviesComponent }];
+
+@NgModule({
+   imports: [RouterModule.forChild(routes)],
+   exports: [RouterModule]
+})
+export class MovieRoutingModule {}
