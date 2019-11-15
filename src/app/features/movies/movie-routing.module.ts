b46fc6024f65d9ca1efa6b1aba29d84998f4ac6f
Anastasia <aeterna_nox@ukr.net>
2019-11-15 13:20:56 +0200

    US-135: Code review - Fixed issues

--- src/app/features/movies/movie-routing.module.ts
+++ src/app/features/movies/movie-routing.module.ts
@@ -1,8 +1,10 @@
-import { MoviesComponent } from './components/movies.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [{ path: '', component: MoviesComponent }];
+import { AuthGuard } from 'src/app/core/guards/auth.guard';
+import { MoviesComponent } from './components/movies.component';
+
+const routes: Routes = [{ path: '', component: MoviesComponent, canActivate: [AuthGuard] }];
 
 @NgModule({
    imports: [RouterModule.forChild(routes)],
